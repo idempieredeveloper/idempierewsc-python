7b4e8ae39558d35d3962d7a9ba8a7cfb5ba4aa9a
vsalazar2017 <victor.salazar@trescloud.com>
2018-07-16 10:30:47 -0500

    [IMP][idempierewsc]: agregando importación req
    agregamos importación para pip >= 10.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,11 @@ along with idempierewsc.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from distutils.core import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 
 requirements = parse_requirements('requirements.txt', session=False)
 
